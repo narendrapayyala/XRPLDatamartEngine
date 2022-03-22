746bb449234468d443146244025ddc6c8f0719b3
Santhosh <santhosh.s@avyata.com>
2022-03-23 00:35:03 +0530

    reporting api

--- controllers/connectors/server/index.js
+++ controllers/connectors/server/index.js
@@ -4,6 +4,7 @@ const moment = require("moment");
 const xrpl = require("xrpl");
 const { entity_model, req_parameters } = require("./entity_model");
 const ReportTemplates = require("../../../db/models").report_templates;
+const { filterFields } = require("../../helper");
 
 router.get("/model", async function (req, res, next) {
   try {
@@ -81,34 +82,55 @@ router.post("/server-info/template", async function (req, res, next) {
   }
 });
 
-// router.get("/server-info/report/:uuid", async function (req, res, next) {
-//   try {
-//     let response = await serverInfo();
-//     return res.status(200).send({ status: true, response });
-//   } catch (err) {
-//     if (err.details) {
-//       return res
-//         .status(400)
-//         .send({ status: false, message: err.details[0].message });
-//     } else {
-//       console.log(err);
-//       return res.status(500).send({
-//         status: false,
-//         message: err.message ? err.message : "Internal Server Error.",
-//       });
-//     }
-//   }
-// });
+router.post("/server-info/fetch", async function (req, res, next) {
+  let template,
+    report_data = [{}];
+  try {
+    if (
+      req.body.id &&
+      req.body.uuid &&
+      req.body.params instanceof Object &&
+      req.body.params
+    ) {
+      template = await ReportTemplates.findOne({ where: { id: req.body.id } });
+      let response = await serverInfo({
+        command: "server_info",
+        ...req.body.params,
+      });
+      template.fields = JSON.parse(template.fields);
+      template.fields.forEach((obj) => {
+        console.log(obj.field_normalized.split("."));
+        report_data[0][obj.field] = filterFields(
+          response.result.info,
+          obj.field_normalized.split(".")
+        );
+      });
+      return res
+        .status(200)
+        .send({ status: true, report_data, template, response });
+    }
+    throw { details: [{ message: "Error! Invalid input data." }] };
+  } catch (err) {
+    if (err.details) {
+      return res
+        .status(400)
+        .send({ status: false, message: err.details[0].message });
+    } else {
+      console.log(err);
+      return res.status(500).send({
+        status: false,
+        message: err.message ? err.message : "Internal Server Error.",
+      });
+    }
+  }
+});
 
 module.exports = router;
 
-async function serverInfo() {
+async function serverInfo(params) {
   const client = new xrpl.Client(process.env.XRPL_WS_CLIENT_ADDRESS);
   await client.connect();
-
-  const response = await client.request({
-    command: "server_info",
-  });
+  const response = await client.request(params);
   client.disconnect();
   return response;
 }
--- controllers/helper.js
+++ controllers/helper.js
@@ -1,22 +1,102 @@
 let obj = {
-  prop1: "value1",
-  prop2: {
-    prop2_1: "value2_1",
-    prop2_2: "value2_2",
-    prop3_3: {
-      prop3_3_1: "value3_3_1",
-      prop3_3_2: "value3_3_2",
+  build_version: "1.8.4",
+  complete_ledgers: "32570-70482195",
+  hostid: "SLUR",
+  initial_sync_duration_us: "26990",
+  io_latency_ms: 1,
+  last_close: {
+    converge_time_s: 0,
+    proposers: 0,
+  },
+  load_factor: 1,
+  pubkey_node: "n9LXeDuna6CEPpm61xXDg7yZKDdnP74iE3y5ShGip7dCSN9oN8nK",
+  published_ledger: "none",
+  reporting: {
+    etl_sources: [
+      {
+        connected: true,
+        grpc_port: "50051",
+        ip: "172.16.2.8",
+        last_message_arrival_time: "2022-Mar-22 17:59:05.533278 UTC",
+        validated_ledgers_range: "68995883-70482195",
+        websocket_port: "51233",
+      },
+      {
+        connected: true,
+        grpc_port: "50051",
+        ip: "172.16.2.136",
+        last_message_arrival_time: "2022-Mar-22 17:59:05.542137 UTC",
+        validated_ledgers_range: "68996401-70482195",
+        websocket_port: "51233",
+      },
+      {
+        connected: true,
+        grpc_port: "50051",
+        ip: "172.16.2.174",
+        last_message_arrival_time: "2022-Mar-22 17:59:05.538015 UTC",
+        validated_ledgers_range: "68997092-70482195",
+        websocket_port: "51233",
+      },
+      {
+        connected: true,
+        grpc_port: "50051",
+        ip: "172.16.2.167",
+        last_message_arrival_time: "2022-Mar-22 17:59:05.533923 UTC",
+        validated_ledgers_range: "68997777-70482195",
+        websocket_port: "51233",
+      },
+    ],
+    is_writer: false,
+    last_publish_time: "2022-Mar-22 17:59:03.383490 UTC",
+  },
+  server_state: "full",
+  server_state_duration_us: "2933643305779",
+  state_accounting: {
+    connected: {
+      duration_us: "0",
+      transitions: "0",
+    },
+    disconnected: {
+      duration_us: "26990",
+      transitions: "1",
+    },
+    full: {
+      duration_us: "2933643305779",
+      transitions: "1",
+    },
+    syncing: {
+      duration_us: "0",
+      transitions: "0",
+    },
+    tracking: {
+      duration_us: "0",
+      transitions: "0",
     },
   },
+  time: "2022-Mar-22 17:59:05.554313 UTC",
+  uptime: 2933647,
+  validated_ledger: {
+    age: 5,
+    base_fee_xrp: 0.00001,
+    hash: "E1FC92239E3A7CC92899A819D2F6C0077D4927A763BE63E72F01C0FCF4921E8F",
+    reserve_base_xrp: 10,
+    reserve_inc_xrp: 2,
+    seq: 70482195,
+  },
+  validation_quorum: 27,
 };
 
-let filter = "prop2.prop3_3.prop3_3_1";
-console.log(getProp(obj, filter.split(".")));
+let filter = "last_close.proposers";
 
-function getProp(obj, props) {
-  if (!obj[props[0]]) return null;
+const filterFields = (obj, props) => {
+  if (obj[props[0]] == undefined) return null;
   if (props.length === 1) {
     return obj ? obj[props[0]] : null;
   }
-  return getProp(obj[props[0]], props.slice(1));
-}
+  return filterFields(obj[props[0]], props.slice(1));
+};
+// console.log(filterFields(obj, filter.split(".")));
+
+module.exports = {
+  filterFields,
+};
