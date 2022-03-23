74d8faa142484bf169e7545ee99bc280122422c8
Santhosh <santhosh.s@avyata.com>
2022-03-23 19:29:33 +0530

    fields sorting fixes

--- controllers/connectors/account/index.js
+++ controllers/connectors/account/index.js
@@ -63,6 +63,57 @@ router.post("/account-info/template", async function (req, res, next) {
   }
 });
 
+router.post("/account-info/fetch", async function (req, res, next) {
+  let template,
+    report_data = [];
+  try {
+    if (
+      req.body.id &&
+      req.body.uuid &&
+      req.body.accounts &&
+      req.body.accounts instanceof Array &&
+      req.body.accounts.length &&
+      req.body.params instanceof Object &&
+      req.body.params
+    ) {
+      template = await ReportTemplates.findOne({ where: { id: req.body.id } });
+      let requests = await req.body.accounts.map((account) =>
+        accountInfo({ account, command: "account_info", ...req.body.params })
+      );
+      let response = await Promise.all(requests);
+      template.fields = JSON.parse(template.fields).sort((a, b) =>
+        a.order > b.order ? 1 : -1
+      );
+      response.forEach((resObj) => {
+        let row = {};
+        template.fields.forEach((obj) => {
+          row[obj.field] = filterFields(
+            resObj.result.info,
+            obj.field_normalized.split(".")
+          );
+        });
+        report_data.push(row);
+      });
+      return res
+        .status(200)
+        .send({ status: true, report_data, template, response });
+    }
+    throw { details: [{ message: "Error! Invalid input data." }] };
+  } catch (err) {
+    if (err.details) {
+      return res
+        .status(400)
+        .send({ status: false, message: err.details[0].message });
+    } else {
+      console.log(err);
+      return res.status(500).send({
+        status: false,
+        message: err.message ? err.message : "Internal Server Error.",
+      });
+    }
+  }
+});
+
 // router.get("/account-info/", async function (req, res, next) {
 //   try {
 //     let response = await serverInfo();
@@ -113,18 +164,10 @@ router.post("/get-balance", async function (req, res, next) {
 
 module.exports = router;
 
-async function getBalance(account) {
+async function accountInfo(params) {
   const client = new xrpl.Client(process.env.XRPL_WS_CLIENT_ADDRESS);
   await client.connect();
-
-  const response = await client.request({
-    command: "account_info",
-    strict: true,
-    account: account,
-    ledger_index: "current",
-  });
-  console.log(response);
-
+  const response = await client.request(params);
   client.disconnect();
   return response;
 }
--- controllers/connectors/server/index.js
+++ controllers/connectors/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const moment = require("moment");
 const xrpl = require("xrpl");
+const CsvParser = require("json2csv").Parser;
 const { v4: uuidv4 } = require("uuid");
 const { entity_model, req_parameters } = require("./entity_model");
 const ReportTemplates = require("../../../db/models").report_templates;
@@ -98,9 +99,10 @@ router.post("/server-info/fetch", async function (req, res, next) {
         command: "server_info",
         ...req.body.params,
       });
-      template.fields = JSON.parse(template.fields);
+      template.fields = JSON.parse(template.fields).sort((a, b) =>
+        a.order > b.order ? 1 : -1
+      );
       template.fields.forEach((obj) => {
-        console.log(obj.field_normalized.split("."));
         report_data[0][obj.field] = filterFields(
           response.result.info,
           obj.field_normalized.split(".")
@@ -109,6 +111,57 @@ router.post("/server-info/fetch", async function (req, res, next) {
       return res
         .status(200)
         .send({ status: true, report_data, template, response });
+    }
+    throw { details: [{ message: "Error! Invalid input data." }] };
+  } catch (err) {
+    if (err.details) {
+      return res
+        .status(400)
+        .send({ status: false, message: err.details[0].message });
+    } else {
+      console.log(err);
+      return res.status(500).send({
+        status: false,
+        message: err.message ? err.message : "Internal Server Error.",
+      });
+    }
+  }
+});
+
+router.post("/server-info/csv", async function (req, res, next) {
+  let template,
+    report_data = [{}],
+    csvFields = [];
+  try {
+    if (
+      req.body.id &&
+      req.body.uuid &&
+      req.body.params instanceof Object &&
+      req.body.params
+    ) {
+      template = await ReportTemplates.findOne({ where: { id: req.body.id } });
+      let response = await serverInfo({
+        command: "server_info",
+        ...req.body.params,
+      });
+      template.fields = JSON.parse(template.fields).sort((a, b) =>
+        a.order > b.order ? 1 : -1
+      );
+      template.fields.forEach((obj) => {
+        csvFields.push(obj.field);
+        report_data[0][obj.field] = filterFields(
+          response.result.info,
+          obj.field_normalized.split(".")
+        );
+      });
+      const csvParser = new CsvParser({ csvFields });
+      const csvData = csvParser.parse(report_data);
+      res.setHeader("Content-Type", "text/csv");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${template.report_name}.csv`
+      );
+      return res.status(200).end(csvData);
     }
     throw { details: [{ message: "Error! Invalid input data." }] };
   } catch (err) {
