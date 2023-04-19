b04a01c7c5022ca99d1e0b2dcad8279eb229a4af
Santhosh <santhosh.s@avyata.com>
2023-04-19 11:39:44 +0530

    Api expose

--- controllers/middlewares.js
+++ controllers/middlewares.js
@@ -1,5 +1,6 @@
 const ConfigSchema = require("../db/models").configs;
 const Users = require("../db/models").users;
+const FirebaseAdmin = require("./firebase-config");
 
 const serverConfig = async (req, res, next) => {
   let config = await ConfigSchema.findOne();
@@ -16,28 +17,35 @@ const verifyToken = async (req, res, next) => {
       res.status(401);
       throw { message: "Error! Access Denied" };
     }
-    let user = await Users.findOne({
-      where: { token: token },
-      raw: true,
-      attributes: { exclude: ["token"] },
-    });
-    if (!user) {
-      res.status(401);
-      throw { message: "Error! Session expired" };
+    const decodeValue = await FirebaseAdmin.auth().verifyIdToken(token);
+    if (decodeValue && decodeValue.email) {
+      let user = await Users.findOne({
+        where: { email: decodeValue.email },
+        raw: true,
+        attributes: { exclude: ["token"] },
+      });
+      if (!user) {
+        res.status(401);
+        throw { message: "Error! User not found" };
+      }
+      req.auth_user = user;
+      let config = await ConfigSchema.findOne({
+        where: { user_id: user.id },
+        raw: true,
+      });
+      if (config) {
+        req.server_config = config;
+        process.env.XRPL_WS_CLIENT_ADDRESS = config.url;
+      }
+      next();
     }
-    req.auth_user = user;
-    let config = await ConfigSchema.findOne({
-      where: { user_id: user.id },
-      raw: true,
-    });
-    if (config) {
-      req.server_config = config;
-      process.env.XRPL_WS_CLIENT_ADDRESS = config.url;
-    }
-    next();
   } catch (err) {
-    return res.send({
+    if (err.message && err.message.includes("Firebase")) {
+      err.message = "Invalid Token";
+    }
+    return res.status(401).send({
       status: false,
+      auth: false,
       message: err.message ? err.message : "Internal Server Error.",
     });
   }
